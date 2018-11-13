a438c3984a71148010cfa9fb81e298b9b84f3d70
maaazahmed <maazahmed2k16@gmail.com>
2018-11-13 22:57:21 +0500

    Updated

--- app.ts
+++ app.ts
@@ -119,3 +119,18 @@
 
 //  var innnerfunc = func("Maaz Ahmed")
 // innnerfunc()
+
+
+function createCounter() {
+    let val = 0;
+    return {
+    increment() { val++ },
+    getVal() { return val }
+    }
+    }
+    let counter = createCounter();
+    counter.increment();
+    console.log(counter.getVal()); // 1
+    counter.increment();
+    console.log(counter.getVal()); // 2
+    
