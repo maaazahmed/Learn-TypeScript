79ec00f5d759eb642d4b1dc58b18d1ff664c68b2
maaazahmed <maazahmed2k16@gmail.com>
2018-11-13 23:36:47 +0500

    Updated

--- app.ts
+++ app.ts
@@ -164,3 +164,32 @@
 // console.log(counter.getVal())
 
 
+
+
+// var Point = (function () {
+//     function Point(x, y) {
+//         this.x = x;
+//         this.y = y;
+//     }
+//     Point.prototype.add = function (point) {
+//         return new Point(this.x + point.x, this.y + point.y);
+//     };
+//     return Point;
+// })();
+
+
+
+class Point {
+    x: number;
+    y: number;
+    constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+    }
+    add(point: Point) {
+    return new Point(this.x + point.x, this.y + point.y);
+    }
+    }
+    var p1 = new Point(0, 10);
+    var p2 = new Point(10, 20);
+    var p3 = p1.add(p2); // {x:10,y:30}
