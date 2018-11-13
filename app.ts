e0b676ee7f91a3a7d086aef8577e63180b077f8f
maaazahmed <maazahmed2k16@gmail.com>
2018-11-13 23:00:31 +0500

    Updated

--- app.ts
+++ app.ts
@@ -130,10 +130,8 @@ function createCounter() {
        getVal() { return val }
      }
     }
-    
     let counter = createCounter();
-    counter.increment();
-    console.log(counter.getVal()); // 1
-    counter.increment();
-    console.log(counter.getVal()); // 2
+    
+    // counter.increment();
+    // console.log(counter.getVal()); // 1
     
